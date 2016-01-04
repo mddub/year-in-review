653a0bfa51875312da20d92711398d6a905adcd4
Mark Wilson <mark@warkmilson.com>
2016-01-04 10:16:59 -0800

    Tiny graphs of event count by month

--- js/view.js
+++ js/view.js
@@ -1,3 +1,6 @@
+/* jshint browser: true */
+/* global $, Mustache */
+
 // e.g. "(december) big sur hike"
 var eventRe = /\((.*?)\)\s+(.*)$/;
 
@@ -5,29 +8,67 @@ var nameRe = /@\w+/g;
 
 var colChoiceTemplate = Mustache.compile($('#col-choice-template').html());
 var categoryTemplate = Mustache.compile($('#category-template').html());
+var monthRowTemplate = Mustache.compile($('#month-row-template').html());
+
+
+var monthFreqs = function(events) {
+  var freqs = [];
+  for(var i = 0; i < 12; i++) {
+    freqs.push(0);
+  }
+
+  // include all relevant month indices from a string like 'jul 27-aug 13'
+  var strs = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
+  events.forEach(function(event) {
+    var earliest = Infinity;
+    var latest = -Infinity;
+    strs.forEach(function(str, i) {
+      if(event.time.toLowerCase().indexOf(str) !== -1) {
+        earliest = Math.min(earliest, i);
+        latest = Math.max(latest, i);
+      }
+    });
+    for(var month = earliest; month <= latest; month++) {
+      freqs[month]++;
+    }
+  });
+
+  return freqs;
+};
+
+var parseEvent = function(el) {
+  if(typeof el !== 'object') {
+    var match = el.match(eventRe);
+    return {
+      time: match[1],
+      event: match[2]
+    };
+  } else if(el.time && el.event) {
+    return el;
+  } else {
+    return undefined;
+  }
+};
 
 var categoryBoxFromData = function(dataset) {
   // formatted data
   var fData = $.extend({}, dataset);
 
-  var fEvents = [];
-  $.each(dataset.entries, function(i, el) {
-    if(typeof el !== 'object') {
-      var match = el.match(eventRe);
-      fEvents.push({
-        time: match[1],
-        event: match[2]
-      });
-    } else {
-      if(el.time && el.event) {
-        fEvents.push(el);
-      }
-    }
-  });
+  var fEvents = dataset.entries
+    .map(parseEvent)
+    .filter(function(e) { return e; });
   fData.events = formatEvents(fEvents);
   fData.count = fEvents.length;
+  fData.months = monthFreqs(fEvents);
 
   return categoryTemplate(fData);
+};
+
+var monthRowFromData = function(dataset) {
+  return monthRowTemplate({
+    title: dataset.title,
+    months: monthFreqs(dataset.entries.map(parseEvent).filter(function(e) { return e; })),
+  });
 };
 
 var formatEvents = function(events) {
@@ -52,8 +93,10 @@ var drawColChoices = function(cols) {
 var drawCollection = function(colName) {
   var collection = window.Review[colName];
   $('.category-box').remove();
+  $('.month-row').remove();
   $.each(collection, function(i, dataset) {
-    $('body').append(categoryBoxFromData(dataset));
+    $('.category-boxes').append(categoryBoxFromData(dataset));
+    $('.month-rows').append(monthRowFromData(dataset));
   });
 };
 
@@ -63,3 +106,8 @@ drawColChoices(cols);
 
 var currentCol = document.location.hash.substr(1) || 'summary';
 drawCollection(currentCol);
+
+$('.toggle-table-link').click(function() {
+  $('.category-boxes, .month-rows').toggle();
+  $('.toggle-table-link').text($('.toggle-table-link').text() === 'table' ? 'boxes' : 'table');
+});
